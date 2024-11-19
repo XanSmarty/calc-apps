70a6bffa0bf75a2df4e6e52f60f54eeb02d89145
Xan Johnson <xan@smarty.com>
2024-11-19 10:30:25 -0700

    Fix csv main

--- main/calc-csv/main.go
+++ main/calc-csv/main.go
@@ -1,8 +1,26 @@
 package main
 
-func main() {
-	//logger := log.New(os.Stderr, ">>> ", 0)
-	//logger := log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-	//handler := handlers.NewCSVHandler()
+import (
+	"log"
+	"os"
 
+	"github.com/XanSmarty/calc-apps/handlers"
+	calc "github.com/XanSmarty/xan-calc-lib"
+)
+
+func main() {
+	logger := log.New(os.Stderr, ">>> ", 0)
+
+	handler := handlers.NewCSVHandler(logger, os.Stdin, os.Stdout, calculators)
+	err := handler.Handle()
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
+
+var calculators = map[string]calc.Calculator{
+	"+": &calc.Addition{},
+	"-": &calc.Subtraction{},
+	"/": &calc.Division{},
+	"*": &calc.Multiplication{},
 }
